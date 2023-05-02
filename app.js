b13cf0c619e3b0477866a00b81f48df31699dc7a
Loren Wu <loren13wu@gmail.com>
2023-05-01 23:50:23 -0700

    fixed navbar and mobile res

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+menuLinks.addEventListener("click", () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
