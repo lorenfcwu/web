2ac91c1a5d39f3f3e0fcd7d5a349ad46234c509d
Loren Wu <loren13wu@gmail.com>
2023-05-03 19:01:00 -0700

    Completed popup and project linking

--- app.js
+++ app.js
@@ -10,3 +10,36 @@ menuLinks.addEventListener("click", () => {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+const openPopupButtons = document.querySelectorAll("[data-popup-target]");
+const closePopupButtons = document.querySelectorAll("[data-popup-close]");
+const overlay = document.getElementById("popup__overlay");
+const popup = document.getElementById("popup");
+
+openPopupButtons.forEach((a) => {
+  a.addEventListener("click", () => {
+    console.log("trying to open");
+    openPopup(popup);
+  });
+});
+
+closePopupButtons.forEach((a) => {
+  a.addEventListener("click", () => {
+    console.log("trying to close");
+    closePopup(popup);
+  });
+});
+
+function openPopup(popup) {
+  console.log("opening");
+  if (popup == null) return;
+  popup.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closePopup(popup) {
+  console.log("closing");
+  if (popup == null) return;
+  popup.classList.remove("active");
+  overlay.classList.remove("active");
+}
